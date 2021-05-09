78bd5c3ff4bd8758cbe8d78a7a4abc303f1d0022
Narazaka <info@narazaka.net>
2021-05-10 05:52:27 +0900

    同時適用修正

--- content.js
+++ content.js
@@ -8,53 +8,72 @@ let options;
 
 /**
  * 
- * @param {ModeType} modeType 
+ * @param {HTMLElement} $item 
+ * @param {[ModeType, Work | Tag][]} wts
  */
-function genApplyStyles(modeType) {
-    const borderProp = joinTagMode(modeType, "border");
-    const borderColorProp = joinTagColorProperty(modeType, "borderColor");
-    const backgroundProp = joinTagMode(modeType, "background");
-    const backgroundColorProp = joinTagColorProperty(modeType, "backgroundColor");
-    const opacityProp = joinTagMode(modeType, "opacity");
-    const hideProp = joinTagMode(modeType, "hide");
-    /**
-     * 
-     * @param {HTMLElement} $item 
-     * @param {Work | Tag} [wt]
-     */
-    function applyStyles($item, wt) {
-        const border = wt && wt.modes[borderProp] ? `2px solid ${wt[borderColorProp]}` : "";
-        if ($item.style.border !== border) $item.style.border = border;
-        const background = wt && wt.modes[backgroundProp] ? wt[backgroundColorProp] : "";
-        if ($item.style.background !== background) $item.style.background = background;
-        const opacity = wt && wt.modes[opacityProp] ? "0.4" : "";
-        if ($item.style.opacity !== opacity) $item.style.opacity = opacity;
-        if (wt && wt.modes[hideProp]) {
-            if ($item.style.display !== "none") {
-                $item.dataset.dlsiteMakerFilterDisplay = $item.style.display;
-                $item.style.display = "none";
-            }
-        } else if ($item.style.display === "none") {
-            $item.style.display = $item.dataset.dlsiteMakerFilterDisplay;
+function applyStyles($item, wts) {
+    let border = "";
+    for (const [modeType, wt] of wts) {
+        const borderProp = joinTagMode(modeType, "border");
+        if (wt && wt.modes[borderProp]) {
+            const borderColorProp = joinTagColorProperty(modeType, "borderColor");
+            border = `2px solid ${wt[borderColorProp]}`;
+            break;
         }
     }
-    return applyStyles;
+    if ($item.style.border !== border) $item.style.border = border;
+
+    let background = "";
+    for (const [modeType, wt] of wts) {
+        const backgroundProp = joinTagMode(modeType, "background");
+        if (wt && wt.modes[backgroundProp]) {
+            const backgroundColorProp = joinTagColorProperty(modeType, "backgroundColor");
+            background = wt[backgroundColorProp];
+            break;
+        }
+    }
+    if ($item.style.background !== background) $item.style.background = background;
+
+    let opacity;
+    for (const [modeType, wt] of wts) {
+        const opacityProp = joinTagMode(modeType, "opacity");
+        if (wt && wt.modes[opacityProp]) {
+            opacity = "0.4";
+            break;
+        }
+    }
+    if ($item.style.opacity !== opacity) $item.style.opacity = opacity;
+
+    let displayNone;
+    for (const [modeType, wt] of wts) {
+        const hideProp = joinTagMode(modeType, "hide");
+        if (wt && wt.modes[hideProp]) {
+            displayNone = true;
+            break;
+        }
+    }
+    if (displayNone) {
+        if ($item.style.display !== "none") {
+            $item.dataset.dlsiteMakerFilterDisplay = $item.style.display;
+            $item.style.display = "none";
+        }
+    } else if ($item.style.display === "none") {
+        $item.style.display = $item.dataset.dlsiteMakerFilterDisplay;
+    }
 }
 
 function filterItems() {
     const $items = /** @type {NodeListOf<HTMLElement>} */(document.querySelectorAll(".n_worklist_item, .search_result_img_box_inner, .n_worklist tr, .push_list > li"));
-    const applyStylesForWork = genApplyStyles("");
-    const applyStylesForTag = genApplyStyles("");
-    const applyStylesForTagWork = genApplyStyles("work");
-    const applyStylesForTagNoWork = genApplyStyles("noWork");
-    
+
     for (const $item of $items) {
         const $makerName = $item.querySelector(".maker_name a");
         const $searchTags = /** @type {NodeListOf<HTMLElement>} */($item.querySelectorAll(".search_tag a"));
+        let work;
+        let noWorkTag;
+        let workTag;
         if ($makerName) {
             const makerName = $makerName.textContent;
-            const work = options.works.find(work => work.makerNamesMap[makerName]);
-            applyStylesForWork($item, work);
+            work = options.works.find(work => work.makerNamesMap[makerName]);
         }
         if ($searchTags) {
             const tagNames = [];
@@ -62,13 +81,12 @@ function filterItems() {
                 const tagName = $searchTag.textContent.trim();
                 tagNames.push(tagName);
                 const tag = options.tags.find(tag => tag.tagsMap[tagName] && tag.modeTypes[""]);
-                applyStylesForTag($searchTag, tag);
+                applyStyles($searchTag, [["", tag]]);
             }
-            const noWorkTag = options.tags.find(tag => tag.modeTypes["noWork"] && tagNames.every(tagName => !tag.tagsMap[tagName]));
-            applyStylesForTagNoWork($item, noWorkTag);
-            const workTag = options.tags.find(tag => tag.modeTypes["work"] && tagNames.some(tagName => tag.tagsMap[tagName]));
-            if (!noWorkTag || workTag) applyStylesForTagWork($item, workTag);
+            noWorkTag = options.tags.find(tag => tag.modeTypes["noWork"] && tagNames.every(tagName => !tag.tagsMap[tagName]));
+            workTag = options.tags.find(tag => tag.modeTypes["work"] && tagNames.some(tagName => tag.tagsMap[tagName]));
         }
+        applyStyles($item, [["", work], ["work", workTag], ["noWork", noWorkTag]]);
     }
 }
 
